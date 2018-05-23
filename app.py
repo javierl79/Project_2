78e316d53b255deca093f181cba6403a730954a9
Jordan Tata <33300481+tatertot53@users.noreply.github.com>
2018-05-22 19:22:21 -0500

    Updated app.py and added models.py

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ from flask import (
     request,
     redirect)
 
+import pymysql
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, inspect
-import pymongo
+from sqlalchemy import create_engine, MetaData, func, inspect, Table
+
+# from models import Value_Batting
 
 import os
 import pandas as pd 
@@ -28,58 +30,92 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tigers2@@6@localhost/baseball'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/battingStatsComplete.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 db = SQLAlchemy(app)
 
+# engine = create_engine('mysql+pymysql://root:tigers2@@6@localhost/baseball')
+# metadata = MetaData(bind=engine)
+# db.create_all()
 
+class Batting(db.Model):
+    __table_args__ = {"autoload": True, "autoload_with": db.engine}
+    __tablename__ = "standardbatting"
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
+class Batting_Advanced(db.Model):
+    __table_args__ = {"autoload": True, "autoload_with": db.engine}
+    __tablename__ = "advancedbatting"
 
-# standardBatting = Base.classes.standard
+# class Batting_Value(db.Model):
+#     __table_args__ = {"autoload": True, "autoload_with": db.engine}
+#     __tablename__ = "valuebatting"
 
-
+# db.create_all()
 
 #################################################
 # Flask Routes
 #################################################
 
-def import_content(filepath):
-    mng_client = pymongo.MongoClient('localhost', 27017)
-    mng_db = mng_client['batter_stats_db'] // Replace mongo db name
-    stats_collection = 'stats_collection' // Replace mongo db collection name
-    db_cm = mng_db[stats_collection]
-    cdir = os.path.dirname(__file__)
-    file_res = os.path.join(cdir, filepath)
-
-    data = pd.read_csv(file_res)
-    data_json = json.loads(data.to_json(orient='records'))
-    db_cm.remove()
-    db_cm.insert(data_json)
-
-if __name__ == "__main__":
-  filepath = '/db/' + filename  // pass csv file path
-  import_content(filepath)
-
 @app.before_first_request
 def setup():
-	db.drop_all()
+	# db.drop_all()
 	db.create_all()
 
-@app.route("/api/batting-data")
+@app.route("/api/standard-batting-data")
 def batting_data():
-	df = pd.read_csv('db/standardBatting.csv')
-	df.to_sql(name = 'standardBatting', con=db.engine, index=False)
-	standard_stats = []
-	for player in session.query(df.Name).all():
-		standard_stats.append(player[0])
+    # standardbatting = Table('standardbatting', metadata, autoload=True)
+    batting_data = []
+    # data = standardbatting.query.all()
+    data = db.session.query(Batting).all()
+    for record in data:
+        standard_dict = {}
+        for col in Batting.__table__.columns:
+            standard_dict[col.name] = getattr(record, col.name)
+        batting_data.append(standard_dict)
 
-	return jsonify(standard_stats)
+    return jsonify(batting_data)
 
+@app.route("/api/advanced-batting-data")
+def advanced_data():
+    advanced_data = []
+    data = db.session.query(Batting_Advanced).all()
+    for record in data:
+        advanced_dict = {}
+        for col in Batting_Advanced.__table__.columns:
+            advanced_dict[col.name] = getattr(record, col.name)
+        advanced_data.append(advanced_dict)
+
+    return jsonify(advanced_data)
+
+# @app.route("/api/value-batting-data/<value_batting>")
+# def value_data():
+#     value_data = []
+#     data = db.session.query(Player).all()
+#     for record in data:
+#         value_dict = {}
+#         value_data.append(value_dict)
+
+
+    # value_data = []
+    # data = db.session.query(Batting_Value).all()
+    # for record in data:
+    #     value_dict = {}
+    #     for col in Batting_Value.__table__.columns:
+    #         value_dict[col.name] = getattr(record, col.name)
+    #     value_data.append(value_dict)
+
+    # return jsonify(value_data)
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+    try:
+        db.session.query('1').from_statement('SELECT 1').all()
+        return '<h1>It works</h1>'
+    except:
+        return '<h1>Something is broken</h1>'
+	# return render_template("index.html")
 
 # @app.route("/stats")
 # def stats():
